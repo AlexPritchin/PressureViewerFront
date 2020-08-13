77e863e010a669682b8cf25a21840d0b0fc8f504
Oleksii Prytchyn <alpritchin@ukr.net>
2020-08-13 18:50:01 +0300

    File entries service: add file entry method added, get file entries method improved

--- lib/services/web_serices/file_entries_service.dart
+++ lib/services/web_serices/file_entries_service.dart
@@ -5,13 +5,24 @@ import 'package:http/http.dart' as http;
 import '../../resources/constants.dart';
 
 class FileEntriesService {
-
   static Future<List<Map<String, dynamic>>> getFileEntries() async {
-    var response = await http.get(serverMainUrl + WebServerUrls.fileEntriesPath);
+    var response =
+        await http.get(serverMainUrl + WebServerUrls.fileEntriesPath);
     if (response.statusCode == 200) {
       return json.decode(response.body).toList();
     }
-    return [];
+    return null;
   }
-  
+
+  static Future<Map<String, dynamic>> addFileEntry(
+      Map<String, dynamic> fileEntryToAdd) async {
+    var response = await http.post(
+        serverMainUrl + WebServerUrls.fileEntriesPath,
+        headers: jsonHeader,
+        body: json.encode(fileEntryToAdd));
+    if (response.statusCode == 201) {
+      return json.decode(response.body)['fileEntry'];
+    }
+    return null;
+  }
 }
