4235e1e4a08e26318dce7c06cc152c4455cd0324
Oleksii Prytchyn <alpritchin@ukr.net>
2020-08-14 16:47:09 +0300

    File entry model updated

--- lib/models/file_entry.dart
+++ lib/models/file_entry.dart
@@ -3,14 +3,14 @@ import 'package:flutter/foundation.dart';
 import '../resources/constants.dart';
 
 class FileEntry {
-  int id;
+  String id;
   DateTime dateModified;
   String fileName;
 
   FileEntry({this.id, @required this.dateModified, @required this.fileName});
 
   FileEntry.fromMap(Map<String, dynamic> entryMap) {
-    if (entryMap[DBFileEntryTableFieldsNames.id] is int) {
+    if (entryMap[DBFileEntryTableFieldsNames.id] is String) {
       id = entryMap[DBFileEntryTableFieldsNames.id];
     }
     if (entryMap[DBFileEntryTableFieldsNames.dateModified] is int) {
