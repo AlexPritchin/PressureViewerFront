deab958fce9c5d343bcff4e69a46bc6534b651bf
Oleksii Prytchyn <alpritchin@ukr.net>
2020-09-02 14:30:05 +0300

    User model modified

--- lib/models/auth/user.dart
+++ lib/models/auth/user.dart
@@ -3,6 +3,20 @@ class User {
   String email;
   String name;
   String password;
+  String confirmPassword;
 
-  User([this.id, this.email, this.password, this.name]);
+  User([this.id, this.email, this.name, this.password, this.confirmPassword]);
+
+  Map<String, dynamic> toMapForLogin() {
+    return {
+      'email': email,
+      'password': password
+    };
+  }
+
+  Map<String, dynamic> toMapForSignup() {
+    var resultingMap = toMapForLogin();
+    resultingMap['confirmPassword'] = confirmPassword;
+    return resultingMap;
+  }
 }
