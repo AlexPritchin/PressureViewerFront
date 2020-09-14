41e7e27ef89f6dd9bc30230712c7fbd101f55e53
Oleksii Prytchyn <alpritchin@ukr.net>
2020-09-14 14:20:36 +0300

    Signup screen added

---
+++ lib/screens/auth/signup_screen.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:connectivity/connectivity.dart';
+
+import '../../models/auth/user.dart';
+import '../../services/data_services/validation_service.dart';
+import '../../helpers/alerts_helper.dart';
+import '../../resources/constants.dart';
+import '../../providers/auth_provider.dart';
+
+class SignupScreen extends StatefulWidget {
+  @override
+  _SignupScreenState createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  bool _isLoading = false;
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  final _emailFocusNode = FocusNode();
+  final _passwordFocusNode = FocusNode();
+  final _confirmPasswordFocusNode = FocusNode();
+  final User userData = User();
+
+  trySignup(BuildContext context) async {
+    _emailFocusNode.unfocus();
+    _passwordFocusNode.unfocus();
+    _confirmPasswordFocusNode.unfocus();
+    var networkConnectionExists = await Connectivity().checkConnectivity();
+    if (networkConnectionExists == ConnectivityResult.none) {
+      AlertsHelper.showSnackBarError(context, Errors.noNetworkError);
+      return;
+    }
+    setState(() {
+      _isLoading = true;
+    });
+    var validationResult = _formKey.currentState.validate();
+    var signupResult = '';
+    if (validationResult) {
+      _formKey.currentState.save();
+      signupResult = await Provider.of<AuthProvider>(context, listen: false)
+          .signup(userData);
+    }
+    setState(() {
+      _isLoading = false;
+    });
+    if (validationResult && signupResult == null) {
+      // Navigator.of(context).pushNamed(routeName)
+    } else if (validationResult && signupResult != null) {
+      AlertsHelper.showSnackBarError(context, signupResult);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Stack(
+        children: [
+          Center(
+            child: Card(
+              elevation: 5.0,
+              child: Container(
+                width: deviceSize.width * 0.75,
+                child: Form(
+                  key: _formKey,
+                  child: SingleChildScrollView(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          decoration: InputDecoration(
+                              labelText: 'E-mail',
+                              labelStyle: TextStyle(fontSize: 14)),
+                          keyboardType: TextInputType.emailAddress,
+                          focusNode: _emailFocusNode,
+                          validator: ValidationService.validateEmail,
+                          onSaved: (value) {
+                            userData.email = value;
+                          },
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                              labelText: 'Password',
+                              labelStyle: TextStyle(fontSize: 14)),
+                          obscureText: true,
+                          focusNode: _passwordFocusNode,
+                          validator: ValidationService.validatePasswordNotEmpty,
+                          onSaved: (value) {
+                            userData.password = value;
+                          },
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                              labelText: 'Confirm password',
+                              labelStyle: TextStyle(fontSize: 14)),
+                          obscureText: true,
+                          focusNode: _confirmPasswordFocusNode,
+                          validator: ValidationService.validatePasswordNotEmpty,
+                          onSaved: (value) {
+                            userData.confirmPassword = value;
+                          },
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        _isLoading
+                            ? RaisedButton(
+                                color: Color.fromRGBO(21, 140, 123, 1),
+                                onPressed: () {
+                                  trySignup(context);
+                                },
+                                child: Text(
+                                  'Sign up',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              )
+                            : CircularProgressIndicator(),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        FlatButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Already have an account? Sign in',
+                            style: TextStyle(color: Colors.blue),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            alignment: Alignment.topCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 60),
+              child: Text(
+                'Pressure viewer',
+                style: TextStyle(
+                  fontSize: 28,
+                  color: Colors.white,
+                  shadows: [
+                    Shadow(
+                      color: Colors.black,
+                      offset: Offset(-3, -1),
+                      blurRadius: 7,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
