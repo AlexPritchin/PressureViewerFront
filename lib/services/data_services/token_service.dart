a405b63e55199747e61eb1b28ad2f8d5b4707a82
Oleksii Prytchyn <alpritchin@ukr.net>
2020-09-21 19:45:30 +0300

    Shared preferences lib Encrypt lib added, Token service implemented

---
+++ lib/services/data_services/token_service.dart
@@ -0,0 +1,30 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:encrypt/encrypt.dart';
+
+class TokenService {
+  static String userToken = '';
+
+  static Future<bool> setRefreshToken(String refreshToken) async {
+    final secretKey = Key.fromUtf8('somesupersecretkey');
+    final initVector = IV.fromUtf8('somesupersecretinitvector');
+    final encrypter = Encrypter(AES(secretKey));
+    final encryptedRefreshToken = encrypter.encrypt(refreshToken, iv: initVector);
+    final sharedPrefs = await SharedPreferences.getInstance();
+    return sharedPrefs.setString('userRefreshToken', encryptedRefreshToken.base64);
+  }
+
+  static Future<String> getRefreshToken() async {
+    final sharedPrefs = await SharedPreferences.getInstance();
+    final base64RefreshToken = sharedPrefs.getString('userRefreshToken');
+    final secretKey = Key.fromUtf8('somesupersecretkey');
+    final initVector = IV.fromUtf8('somesupersecretinitvector');
+    final encrypter = Encrypter(AES(secretKey));
+    final encryptedRefreshToken = Encrypted.fromBase64(base64RefreshToken);
+    return encrypter.decrypt(encryptedRefreshToken, iv: initVector);
+  }
+
+  static Future<bool> clearRefreshToken() async {
+    final sharedPrefs = await SharedPreferences.getInstance();
+    return sharedPrefs.remove('userRefreshToken');
+  }
+}
